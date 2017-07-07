c6d5474631fe0b02f8be375c4998271cda8af0a2
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-07-07 15:32:36 -0700

    seeded database with admin id and password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+admin_list = [
+  [ "admin@admin.com", "password"],
+]
+
+admin_list.each do |email, password|
+  User.create( email: email, password: password, admin: true )
+end
+
+p "Created #{Service.count} services"
+p "Created #{User.count} admin"
