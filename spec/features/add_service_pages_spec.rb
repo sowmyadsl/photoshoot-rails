6b72249fd27e658609ea0652497d0475132c367e
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-07-08 10:39:56 -0700

    Pass integration test for admin adding a product without entering the name field

--- spec/features/add_service_pages_spec.rb
+++ spec/features/add_service_pages_spec.rb
@@ -11,4 +11,17 @@ feature "Sessions" do
     click_button "New Service"
     expect(page).to have_content("New Service")
   end
+
+  scenario "displays an error if a service name was missing" do
+    admin = create(:admin)
+    sign_in admin
+    service = build(:service)
+    visit  new_service_path(admin)
+    fill_in 'Description', with: service.description
+    fill_in 'Image', with: service.image
+    fill_in 'Price', with: service.price
+    click_button 'Create Service'
+    expect(page).to have_content("Name can't be blank")
+  end
+
 end
