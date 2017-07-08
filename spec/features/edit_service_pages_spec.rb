c86cbfef42176375bfdca63792317fefbba21a90
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-07-08 12:30:21 -0700

    Add integration specs for admin editing service page

---
+++ spec/features/edit_service_pages_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "Sessions" do
+  scenario "admin edits  a service" do
+    admin = create(:admin)
+    sign_in admin
+    service = create(:service)
+    visit edit_service_path(service)
+    fill_in 'service_name', with: "Edited Name"
+    click_button 'Update Service'
+    expect(page).to have_content("Edited Name")
+  end
+
+  scenario "renders edit page if there is an error" do
+    admin = create(:admin)
+    sign_in admin
+    service = create(:service)
+    visit edit_service_path(service)
+    fill_in 'service_name', with: ""
+    click_button 'Update Service'
+    expect(page).to have_content("error")
+  end
+end
